a6162cf0ad57066f03ba3b207963a865707664e1
Moro-Afriyie <ceremoinc99@gmail.com>
2022-10-16 22:47:45 +0000

    update

--- app.js
+++ app.js
@@ -9,17 +9,27 @@ const port = 8080;
 
 app.use(cors());
 
-const scrappingURL = 'https://thejellybee.com/';
+app.use(express.json());
 
-async function generateStyle(item) {
+async function generateStyle(item, baseUrl) {
 	// get all the css codes from the websites and create a style tag for it.. this might take a while to complete depending on the size of the array
 	const response = await axios.get(item);
 	const data = response.data;
-	return `<style>${data}</style>`;
+	// check if the link is equal to the base url and copies the head tags because some fonts werent downloaded
+	if (item === baseUrl) {
+		const $ = cheerio.load(response.data);
+		$('script').remove();
+		$('meta').remove();
+		$('noscript').remove();
+		console.log('data: ', $('head').html());
+		return `<style>${$('head').html()}</style>`;
+	} else {
+		return `<style>${data}</style>`;
+	}
 }
 
-app.get('/', async (req, res) => {
-	const response = await axios.get(scrappingURL);
+app.post('/', async (req, res) => {
+	const response = await axios.get(req.body.website);
 	const websiteHtml = response.data;
 	const $ = cheerio.load(websiteHtml);
 	const links = [];
@@ -28,12 +38,7 @@ app.get('/', async (req, res) => {
 	$('head')
 		.find('link')
 		.attr('href', (item, elem) => {
-			if (
-				(elem.includes('cdn') || elem.includes('.css')) &&
-				!elem.includes('.png') &&
-				!elem.includes('.judge') &&
-				!elem.includes('cdnjs')
-			) {
+			if (!elem.includes('.png') && !elem.includes('.judge') && !elem.includes('cdnjs')) {
 				if (elem.includes('https') || elem.includes('http')) {
 					links.push(elem);
 				} else {
@@ -42,12 +47,17 @@ app.get('/', async (req, res) => {
 			}
 		});
 
-	const responseFromPromise = await Promise.all([...links.map((item) => generateStyle(item))]);
+	const responseFromPromise = await Promise.all([
+		...links.map((item) => generateStyle(item, req.body.website)),
+	]);
 
 	res.json({
 		success: true,
-		website: scrappingURL,
-		data: '<style>body, html{height: auto !important;}</style>' + responseFromPromise.join(''),
+		website: req.body.website,
+		links: links,
+		data:
+			`<style>.product-grid-item-title{width:max-content !important}</style>` +
+			responseFromPromise.join(''),
 	});
 });
 
